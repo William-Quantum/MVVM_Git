7e79c3ac53eb6ac06c6dd63f1cfc9b51b47cb415
William-Quantum <phamphuthien23111999@gmail.com>
2022-02-15 09:57:54 +0700

    Connected

---
+++ MVVM/MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by mac on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
